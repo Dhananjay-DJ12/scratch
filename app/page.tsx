202572a74762ca62d2bcd0426bf608353d363e68
Dhananjay-DJ12 <thakurraaju349@gmail.com>
2026-01-18 21:52:23 +0530

    fix: correct dynamic import in page.tsx and remove output config in next.config.mjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-import dynamicImport from "next/dynamic";
+import dynamic from "next/dynamic";
 
 /**
  * disable ssr to avoid pre-rendering issues of Next.js
  *
  * we're doing this because we're using a canvas element that can't be pre-rendered by Next.js on the server
  */
-const App = dynamicImport(() => import("./App"), { ssr: false });
-
-export const dynamic = "force-dynamic";
+const App = dynamic(() => import("./App"), { ssr: false });
 
 export default App;
  
