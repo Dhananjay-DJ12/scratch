a3fe826d094a4c3e8230bbaece9eb0d35ff5158c
Dhananjay-DJ12 <thakurraaju349@gmail.com>
2026-01-18 22:10:27 +0530

    Fix merge conflict and dynamic import in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
 import dynamic from "next/dynamic";
 
 /**
- * disable ssr to avoid pre-rendering issues of Next.js
- *
- * we're doing this because we're using a canvas element that can't be pre-rendered by Next.js on the server
+ * We disable SSR because the App uses canvas
+ * which cannot be rendered on the server.
  */
-<<<<<<< HEAD
 const App = dynamic(() => import("./App"), { ssr: false });
-=======
-const App = dynamicImport(() => import("./App"), { ssr: false });
-
-export const dynamic = "error";
->>>>>>> d977252529ec070ea20a999d5fcd32ede15563d7
 
 export default App;
- 
